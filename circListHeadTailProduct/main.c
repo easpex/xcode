a4ab32b6bc0aaf3ec6a8b4e5115ae4078bd77d86
easpex <subsinfoapps@gmail.com>
2017-02-03 15:16:42 +0200

    zdczxczcz

--- circListHeadTailProduct/main.c
+++ circListHeadTailProduct/main.c
@@ -13,14 +13,18 @@ void printList(ptr p);
 int main() {
     ptr head = NULL;
 
-    add2list(&head, 2);
-    add2list(&head, 3);
-    add2list(&head, 4);
-    add2list(&head, 5);
     add2list(&head, 1);
     add2list(&head, 2);
+    add2list(&head, 5);
+    add2list(&head, 3);
+    add2list(&head, 0);
+    add2list(&head, 4);
+    add2list(&head, -2);
 
-    printf("head = %d\n", head -> next -> data);
+    
+
+
+    printf("head = %d\n", head -> data);
     printList(head);
     
     return 0;
@@ -29,7 +33,7 @@ int main() {
 
 void add2list(ptr *head, int num) {
     printf("inside add\n");
-    ptr p1, p2, t, temp;
+    ptr p1, p2, t;
     
     t = (ptr) malloc(sizeof(item));
     
@@ -41,39 +45,45 @@ void add2list(ptr *head, int num) {
     t -> data = num;
     p1 = *head;
     
-    printf("p1 != NULL = %d\n", p1 != NULL);
-    if(p1) {
-        printf("p1 -> next != *head = %d\n", p1 -> next != *head);
-        printf("p1 -> data < num = %d\n", p1 -> data < num);
-    }
-    
-    
-    
     while(p1 != NULL && p1 -> next != *head && p1 -> data < num) {
-        printf("p1 != NULL = %d\n", p1 != NULL);
-        printf("p1 -> next != *head = %d\n", p1 -> next != *head);
-        printf("p1 -> data < num = %d\n", p1 -> data < num);
-        
         p2 = p1;
         p1 = p1 -> next;
     }
     
-    if(*head == p1) {
+    if(!p1) { //case 1 - if the list is empty
+        *head = t;
+        t -> next = *head;
+    } else if(p1 -> data < t -> data) {
+        //if we need to add a node to the end of the list
+        printf("case 2|p1 -> data < t -> data = %d\n", t -> data);
+        p1 -> next = t;
+        t -> next = *head;
+    }else if(p1 == *head) {
+
+        //if we need to add a node to the beginning of the list
+        printf("case 3|p1 -> data >= t -> data = %d\n", t -> data);
+        while(p1 -> next != *head) {
+            p1 = p1 -> next;
+        }
+        p1 -> next = t;
         t -> next = *head;
         *head = t;
     } else {
+        //need to add a node in the middle
+        printf("case 4|p1 -> data >= t -> data = %d\n", t -> data);
         p2 -> next = t;
         t -> next = p1;
     }
 }
 
-
- void printList(ptr head) {
-	printf("inside print");
-	ptr tmp;
-	tmp = head;
-	while(tmp -> next != head) {
-        printf("%d ->\n", tmp -> data);
+void printList(ptr head) {
+    ptr tmp;
+    printf("inside print\n");
+    
+    tmp = head;
+    while(tmp -> next != head) {
+        printf("%d -> ", tmp -> data);
         tmp = tmp -> next;
-	}
- }
+    }
+    printf("%d\n", tmp -> data);
+}
